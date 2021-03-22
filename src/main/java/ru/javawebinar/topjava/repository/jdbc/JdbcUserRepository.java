7762e1805842fbaddfc4d6366476d0f4109c6649
intruder <79538840974@ya.ru>
2021-03-22 13:28:52 +0700

    Add validation for jdbc

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -4,16 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import javax.validation.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 @Repository
+@Transactional
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
@@ -35,7 +43,14 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<User>> validate = validator.validate(user);
+        if (validate.size() > 0) {
+            throw new ConstraintViolationException(validate);
+        }
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
 
         if (user.isNew()) {
@@ -51,13 +66,36 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = jdbcTemplate.query("SELECT * FROM users left join user_roles ur on users.id = ur.user_id WHERE id=?",
+                (ResultSetExtractor<List<User>>) resultSet -> {
+                    HashMap<Integer, User> hashM = new HashMap<>();
+
+                    while (resultSet.next()) {
+                        int userId = resultSet.getInt("id");
+                        User user = hashM.get(userId);
+                        Role role = Role.valueOf(resultSet.getString("role"));
+                        if (user != null) {
+                            user.getRoles().add(role);
+                        } else {
+                            hashM.put(userId, new User(resultSet.getInt("id"),
+                                    resultSet.getString("name"),
+                                    resultSet.getString("email"),
+                                    null,
+                                    resultSet.getInt("calories_per_day"),
+                                    resultSet.getBoolean("enabled"),
+                                    resultSet.getDate("registered"),
+                                    Set.of(role)));
+                        }
+                    }
+                    return new ArrayList<>(hashM.values());
+                }, id);
         return DataAccessUtils.singleResult(users);
     }
 
